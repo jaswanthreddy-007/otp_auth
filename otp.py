23a9597df8d872013ce8ec4b734bb3a330f81605
jaswanthreddy-007 <88081034+jaswanthreddy-007@users.noreply.github.com>
2022-12-04 20:05:53 +0530

    encoded otp generation

--- otp.py
+++ otp.py
@@ -1,29 +1,35 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
+
 import os
 from twilio.rest import Client
 import random as rd
-# Find your Account SID and Auth Token in Account Info and set the environment variables.
-# See http://twil.io/secure
+import pyotp
+
+
+totp = pyotp.TOTP("JBSWY3DPEHPK3PXP")
+filename = 'otp_auth/twilliokey.txt'
 account_sid = 'AC8247b456a32ed3486802b73ef45b6a6b'
-auth_token = 'a4931f421460e2cc23601c0767d3ec47'
+try:
+        with open(filename, 'r') as f:
+            auth_token= f.read().strip()
+except FileNotFoundError:
+    print("'%s' file not found" % filename)
 client = Client(account_sid, auth_token)
 
-rd.seed()
-def sendotp():
-    otp = rd.randint(1000,9999)
 
-    message = client.messages.create(body='Hi there your otp is '+str(otp),
+def sendotp():
+    otp = totp.now()
+
+    message = client.messages.create(body='Hi there your otp is '+otp,
                                     from_='+13608032876',
                                     to='+918688833274'
                                     )
     return otp
 otp = sendotp()
 input_otp = int(input('enter otp  :  '))
-if otp==input_otp:
+if totp.verify(input_otp):
     print('access granted')
 else:
     print('access denied')
-    print('try again')
-    sendotp()
+    
     
 
